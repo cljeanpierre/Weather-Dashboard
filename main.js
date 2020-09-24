20ac715d68b6a34bb8c8b4b4dc54ff04fd37a142
Carla <carla@layc-dc.org>
2020-09-23 23:51:54 -0400

    added function to retrieve data for cityInput

--- main.js
+++ main.js
@@ -1,50 +1,59 @@
 
 
 $(document).ready(function () {
- // This is my API key
- var APIKey = "e2430c230d3e4cfb83bfa51840a196d3";
- //These are the URLs needed to query the database
- var queryURL = "https://api.openweathermap.org/data/2.5/weather?appid=" + APIKey + "&q="
- 
- var queryForecast = "https://api.openweathermap.org/data/2.5/forecast?appid=" + APIKey + "&q="
+    // This is my API key
+    var APIKey = "e2430c230d3e4cfb83bfa51840a196d3";
+    //These are the URLs needed to query the database
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?appid=" + APIKey + "&q="
 
- var queryIndex = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey +"&q="
- // APIKey {appid}&lat={lat}&lon={lon}
+    var queryForecast = "https://api.openweathermap.org/data/2.5/forecast?appid=" + APIKey + "&q="
 
- $('#city-form').on('submit', function (event) {
-event.preventDefault();
-    console.log("click button")
-    var cityInput=$('#city-input').val()
-    console.log("city input")
-    $('#citylist').append(`<li class="list-group-item">${cityInput}</li>`)
+    var queryIndex = "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey + "&q="
+    // APIKey {appid}&lat={lat}&lon={lon}
+
+    $('#city-form').on('submit', function (event) {
+        event.preventDefault();
+        console.log("click button")
+        var cityInput = $('#city-input').val()
+        console.log("city input")
+        $('#citylist').append(`<li class="list-group-item">${cityInput}</li>`)
 
 
-// Run AJAX call to the OpenWeatherMap API
-  $.ajax({
- url: queryURL + cityInput,
-  method: "GET"
-  })
- // Store all of the retrieved data inside of an object called "response"
- .then(function(response) {
+        // Run AJAX call to the OpenWeatherMap API
+        $.ajax({
+            url: queryURL + cityInput,
+            method: "GET"
+        })
+        $.ajax({
+            url: queryForecast + cityInput,
+            method: "GET"
+        })
+        $.ajax({
+            url: queryIndex + cityInput,
+            method: "GET"
+        })
+            // Store all of the retrieved data inside of an object called "results"
+            .then(function (results) {
+                console.log(results);
+                var initialResults = results.response.docs;
+                console.log(initialResults);
+                for (var i = 0; i < initialResults.length; i++) {
+                    var articleDiv = $("<div>");
+                    var h = $("<h3>").text(results[i].main);
+                    console.log(h);
+                    articleDiv.append(h);
+                    $("#city-input").append (articleDiv);
+                }
 
- // Log the queryURLs
- console.log(queryURL);
- console.log(queryForecast);
- console.log(queryIndex);
+                // // Transfer content to HTML
+                // $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+                // $(".wind").text("Wind Speed: " + response.wind.speed);
+                // $(".humidity").text("Humidity: " + response.main.humidity);
 
- // Log the resulting object
- console.log(response);
+                // Log the data in the console as well
+                // console.log("Wind Speed: " + response.wind.speed);
+                // console.log("Humidity: " + response.main.humidity);
+                // console.log("Temperature (F): " + tempF);
+            });
 
- // // Transfer content to HTML
- // $(".city").html("<h1>" + response.name + " Weather Details</h1>");
- // $(".wind").text("Wind Speed: " + response.wind.speed);
- // $(".humidity").text("Humidity: " + response.main.humidity);
- 
- // Log the data in the console as well
- // console.log("Wind Speed: " + response.wind.speed);
- // console.log("Humidity: " + response.main.humidity);
- // console.log("Temperature (F): " + tempF);
-});
-
-});
-});
+    });

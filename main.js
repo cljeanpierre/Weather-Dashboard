c28004b97d86e673f9525523572a3a2a768a2c7f
Carla <carla@layc-dc.org>
2020-10-11 17:47:34 -0400

    updated function with temp converter

--- main.js
+++ main.js
@@ -8,21 +8,28 @@ var queryForecast = "https://api.openweathermap.org/data/2.5/forecast?" + APIKey
 var queryIndex = "http://api.openweathermap.org/data/2.5/uvi?" + APIKey + "&q="
 // APIKey {appid}&lat={lat}&lon={lon}
 
-function currentWeather(cityInput){
+
+function currentWeather(cityInput) {
     $.ajax({
         type: "GET",
         url: queryURL + cityInput
-    }).then(function(results){
+    }).then(function (results) {
         console.log(results);
         console.log(results.main.temp);
         console.log(results.main.humidity);
         console.log(results.wind.speed);
 
-        var content = 
+        var content =
         `<h4>Temperature = ${results.main.temp}</h4>
         <h4>Humidity = ${results.main.humidity}</h4>
         <h4>Wind Speed = ${results.wind.speed}</h4>`;
 
+        function tempConvert () {
+            var kelvin = Temperature;
+            var celsius = Temperature - 273;
+            var fahrenheit = Math.floor(celsius * (9/5) + 32);
+            console.log(tempConvert);
+        }
 
         $("#currentTempHumWin").html(content);
         // forecast(cityInput);
@@ -31,36 +38,53 @@ function currentWeather(cityInput){
 
     })
 }
-
-function forecast(cityInput){
+function forecast(cityInput) {
     $.ajax({
         type: "GET",
         url: queryForecast + cityInput
-    }).then(function(results){
+    }).then(function (results) {
         console.log(results);
-        for (var i = 0; i < results.length; i+=8);
-        
-        var fiveDays = 
-        `<span id="monTemp"> ${results.list[i].main.temp}</span>
-        <span id="monHum"> ${results.list[i].main.humidity}</span>`;
+        for (var i = 0; i < results.length; i += 8);
 
-        console.log("fiveDays: ", fiveDays);
+        var fiveDaysTemp = `<span> ${results.list[i].main.temp}</span>`;
+        var fiveDaysHum = `<span> ${results.list[i].main.humidity}</span>`;
+        var tuesdayTemp = `<span> ${results.list[i].main.temp}</span>`;
+        var tuesdayHum = `<span> ${results.list[i].main.humidity}</span>`;
+        var wedTemp = `<span> ${results.list[i].main.temp}</span>`
+        var wedHum = `<span> ${results.list[i].main.humidity}</span>`
+        var thursTemp = `<span> ${results.list[i].main.temp}</span>`
+        var thursHum = `<span> ${results.list[i].main.humidity}</span>`
+        var friTemp = `<span> ${results.list[i].main.temp}</span>`
+        var friHum = `<span> ${results.list[i].main.humidity}</span>`
+
+        console.log("fiveDays: ", fiveDaysTemp);
         console.log("results.list: ", results.list);
         console.log("results.list[i].main.temp: ", results.list[i].main.temp);
+        console.log("HERE", $("#monTemp"))
 
-        $("#monTemp").html(fiveDays);
-        $("#monHum").html(fiveDays);
+        $("#monTemp").html(fiveDaysTemp);
+        $("#monHum").html(fiveDaysHum);
+        $("#tuesTemp").html(tuesdayTemp);
+        $("#tuesHum").html(tuesdayHum);
+        $("#wedTemp").html(wedTemp);
+        $("#wedHum").html(wedHum);
+        $("#thursTemp").html(thursTemp);
+        $("#thursHum").html(thursHum);
+        $("#friTemp").html(friTemp);
+        $("#friHum").html(friHum);
+
+
     })
-    
+
 };
 
-function uvIndex(lat, lon){
-//set lat, lon = cityInput
+function uvIndex(lat, lon) {
+    //set lat, lon = cityInput
 }
 
 
 $(document).ready(function () {
-    
+
     $('#city-form').on('submit', function (event) {
         event.preventDefault();
         console.log("This button has been clicked")
@@ -70,7 +94,7 @@ $(document).ready(function () {
 
         currentWeather(cityInput);
         forecast(cityInput);
-    
+
 
 
     });
